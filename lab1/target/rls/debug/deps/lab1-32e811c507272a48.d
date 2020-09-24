81118790db5dced84dbdc77562929c4782f71adb
Raul <rquispeto@unsa.edu.pe>
2020-09-23 21:11:08 -0500

    add lab1

---
+++ lab1/target/rls/debug/deps/lab1-32e811c507272a48.d
@@ -0,0 +1,5 @@
+/home/serlog/Escritorio/Raúl/Semestre-2020-II/Paralelos/parallel_computing/lab1/target/rls/debug/deps/lab1-32e811c507272a48.rmeta: src/main.rs
+
+/home/serlog/Escritorio/Raúl/Semestre-2020-II/Paralelos/parallel_computing/lab1/target/rls/debug/deps/lab1-32e811c507272a48.d: src/main.rs
+
+src/main.rs:
